8d84da776de74a4c87052694caaddbc39188fb7e
yukioichida <yukioichida@gmail.com>
2017-09-03 13:59:01 -0300

    datasets

--- run_model.py
+++ run_model.py
@@ -1,19 +1,18 @@
 from modules.log_config import LOG
 from modules.input_data import prepare_input_data
-from modules.datasets import STSDataset
-
-from gensim.models.keyedvectors import KeyedVectors
+from modules.datasets import *
 
 import numpy as np
-from time import time
 import datetime
+from sklearn.model_selection import train_test_split
+from time import time
+
+from gensim.models.keyedvectors import KeyedVectors
 
 from keras.optimizers import Adadelta
 from keras.models import Model
 from keras.layers import Dense, Input, Embedding, Dropout, Activation, Merge
 from keras.layers.recurrent import GRU, LSTM
-from keras.layers.merge import concatenate
-from sklearn.model_selection import train_test_split
 from keras import backend as K
 
 MAX_NB_WORDS = 60000
@@ -22,23 +21,48 @@ LSTM_HIDDEN_LAYERS=50 # by the paper
 BATCH_SIZE = 100
 
 QUORA_FILE = 'C:\\dev_env\\ml\\datasets\\quora_questions_pair\\train.csv'
-STS_FILE = 'C:\\dev_env\\ml\\datasets\\sts\\sts_all.txt'
+#STS_FILE = 'C:\\dev_env\\ml\\datasets\\sts\\sts_all.txt'
+STS_FILE = 'C:\\dev_env\\ml\\datasets\\sts\\sts-processed.tsv'
+
 SICK_FILE = 'C:\\dev_env\ml\\datasets\\sick_2014\\SICK_complete.txt'
 WORD2VEC = 'C:\dev_env\ml\datasets\GoogleNews-vectors-negative300.bin\\GoogleNews-vectors-negative300.bin'
 GLOVE = 'C:\dev_env\ml\datasets\glove.6B\\glove.6B.300d.gensim.txt'
 
 EMBEDDING_FILE = WORD2VEC
 
-dataframe = STSDataset(STS_FILE).data_frame()
+pretrain_dataframe = SICKDataset(SICK_FILE).data_frame()
+train_dataframe = STSDataset(STS_FILE).data_frame()
+
+
 input_data = prepare_input_data(dataframe, rescaling_output=5)
 
+
 max_sentence_length = input_data.max_sentence_length
-vocab_size = input_data.vocab_size
+vocab_size = input_data.vocab_size + 1
 word_index = input_data.word_index
 
+#=======================================
+#   EMBEDDING MATRIX FOR WORD EMBEDDINGS
+#=======================================
+LOG.info('Loading embedding model from %s', EMBEDDING_FILE)
+embedding_model = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
+#embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+embedding_matrix = 1 * np.random.randn(vocab_size, EMBEDDING_DIM)  # This will be the embedding matrix
+embedding_matrix[0] = 0  # So that the padding will be ignored
+LOG.info('Creating the embedding matrix')
+for word, idx in word_index.items():
+    if idx >= vocab_size:
+        continue
+    if word in embedding_model.vocab:
+        embedding_vector = embedding_model.word_vec(word)
+        if embedding_vector is not None:
+            embedding_matrix[idx] = embedding_vector
 
-
-
+LOG.info('Embedding matrix as been created, removing embedding model from memory')
+del embedding_model
+# =========================================
+# ============= MODEL =====================
+# =========================================
 
 def exponent_neg_manhattan_distance(left, right):
     ''' Helper function for the similarity estimate of the LSTMs outputs'''
@@ -46,7 +70,6 @@ def exponent_neg_manhattan_distance(left, right):
 
 LOG.info("Creating model...")
 
-# ============= MODEL =====================
 # A entrada recebe os índices das palavras no vocabulário, para fazer o lookup na tabela de embeddings
 left_input = Input(shape=(max_sentence_length,), dtype='int32')
 right_input = Input(shape=(max_sentence_length,), dtype='int32')
@@ -61,7 +84,7 @@ left_encoder = embedding_layer(left_input)
 right_encoder = embedding_layer(right_input)
 
 # LSTM
-base_lstm = GRU(LSTM_HIDDEN_LAYERS)
+base_lstm = LSTM(LSTM_HIDDEN_LAYERS)
 
 left_representation = base_lstm(left_encoder)
 right_representation = base_lstm(right_encoder)
@@ -80,14 +103,23 @@ malstm.compile(loss = 'mean_squared_error',
                optimizer=optimizer,
                metrics=['accuracy'])
 
+# =====================================
+# ============= PRE TRAIN =============
+# =====================================
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(input_data.x1, input_data.x2, input_data.y,
+                                                                         test_size=0.2, random_state=42)
 training_time = time()
-EPOCHS = 200
+EPOCHS = 20
 malstm.fit([x1_train, x2_train], y_train,
            epochs= EPOCHS,
            batch_size=BATCH_SIZE,
            validation_data=([x1_test, x2_test], y_test))
 
-print("\nTraining time finished.\n{} epochs in {}".format(EPOCHS, datetime.timedelta(seconds=time()-training_time)))
+print("\nPré Training time finished.\n{} epochs in {}".format(EPOCHS, datetime.timedelta(seconds=time()-training_time)))
 
 score, acc = malstm.evaluate([x1_test, x2_test], y_test, batch_size=BATCH_SIZE)
 print("\nTest score: %.3f, accuracy: %.3f" % (score, acc))
+
+# =====================================
+# ============= TRAIN =============
+# =====================================
